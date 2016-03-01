4a543e6d604697d2c84975782e70be08f67f9aab
dave <devscrapper@yahoo.fr>
2016-03-01 22:12:18 +0100

    v0.16 ajout d'un item de munipour acceder au Calzndar de enginebot en attendant d'appeler des ressources et de faire une truc plus joli interdire la publication de policy si le monday start est passé ajout dans lindex e website des infos d'existence de keyword et de backlink pour le website correction du bug d'affichage du calendrier qui ne saffichait pas qd on clique sur l'icone en pecher la publication d'une poliyc traffic quan dle monday start est depassé controle que l'url_root d'un website est bien formée controle que l'url_root est joignable

--- app/models/website.rb
+++ app/models/website.rb
@@ -1,5 +1,5 @@
 require 'addressable/uri'
-
+require 'rest-client'
 class Website < ActiveRecord::Base
   include Addressable
   serialize :schemes, Array
@@ -7,21 +7,28 @@ class Website < ActiveRecord::Base
   serialize :advertisers, Array
   has_many :traffics
   has_many :ranks
-  validates :label , :presence => true
+  validates :label, :presence => true
   validates :count_page, :presence => true, :numericality => {:only_integer => true, :greater_than_or_equal_to => 0}
   validate :check_url
 end
 
 def check_url
   begin
-   uri = URI.parse(url_root)
-   raise if uri.scheme.nil? or
-              uri.hostname.nil? or
-              uri.path.empty?
+    uri = URI.parse(url_root)
+    raise if uri.scheme.nil? or
+        uri.hostname.nil? or
+        uri.path.empty?
   rescue
     errors.add(:url_root, " : <#{url_root}> is malformmed")
   else
+    begin
+      RestClient.head(url_root)
+    rescue Exception => e
+      errors.add(:url_root, "can join website")
+    else
+
     end
+  end
 end
 
 def self.search(search)
