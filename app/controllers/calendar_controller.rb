2a878ccf5557fd0c35c15570f98394cae82b7450
dave <devscrapper@yahoo.fr>
2016-03-24 18:22:08 +0100

    v0.19
    ajout du mode d'exécution des task du calendar en mode auto ou manual en fonction de la propriété de la policy

---
+++ app/controllers/calendar_controller.rb
@@ -0,0 +1,74 @@
+require_relative '../../lib/calendar'
+class CalendarController < ApplicationController
+
+  def execute
+    begin
+      Calendar::execute_task(params['task_label'], params['task_id'])
+
+    rescue Exception => e
+      @alert = "Calendar not start execution task #{params['task_label']} : #{e.message}"
+    else
+      @notice = "Calendar start execution task #{params['task_label']}"
+    ensure
+      @tasks = Calendar::current_date(params['current_date'], params['policy_id'], params['policy_type'])
+      @policy_type =params['policy_type']
+      @policy_id = params['policy_id']
+      @current_date = params['current_date']
+    end
+  end
+
+  def prev_day
+    begin
+      @tasks = Calendar::prev_day(params['current_date'], params['policy_id'], params['policy_type'])
+
+    rescue Exception => e
+      @alert = "cannot get previous day task : #{e.message}"
+    else
+    ensure
+      @policy_type =params['policy_type']
+      @policy_id = params['policy_id']
+      @current_date = Date.parse(params['current_date']).prev_day
+    end
+  end
+
+  def today
+    begin
+      @tasks = Calendar::today(params['policy_id'], params['policy_type'])
+
+    rescue Exception => e
+      @alert = "cannot get today task : #{e.message}"
+    else
+    ensure
+      @policy_type =params['policy_type']
+      @policy_id = params['policy_id']
+      @current_date = Date.today
+    end
+  end
+
+  def next_day
+    begin
+      @tasks = Calendar::next_day(params['current_date'], params['policy_id'], params['policy_type'])
+
+    rescue Exception => e
+      @alert = "cannot get next day task : #{e.message}"
+    else
+    ensure
+      @policy_type =params['policy_type']
+      @policy_id = params['policy_id']
+      @current_date = Date.parse(params['current_date']).next_day
+    end
+  end
+
+  private
+
+
+  def calendar_params
+    params.require(:calendar).permit(:current_date,
+                                     :policy_id,
+                                     :policy_type,
+                                     :task_id,
+                                     :task_label)
+  end
+
+end
+
