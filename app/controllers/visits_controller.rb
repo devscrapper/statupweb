07aa31c0e95b9f2c08c41884331d2662845c209b
dave <devscrapper@yahoo.fr>
2016-05-13 17:53:08 +0200

    v0.34
    ajout de tri sur browser et geolocation dans la console des visits

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -118,7 +118,15 @@ class VisitsController < ApplicationController
     end
   end
 
-
+  def order_by
+    @visits = Visit.where({:policy_id => params[:policy_id],
+                           :policy_type => params[:policy_type],
+                           :state => params[:state]}).order("#{params[:criteria].to_s || "start_time"} #{params[:way] || "desc"}")
+    @policy_id = params['policy_id']
+    @policy_type = params['policy_type']
+    @execution_mode = params['execution_mode']
+    @state = params[:state]
+  end
   def publish
     begin
       Scheduler::publish(params['visit_id'])
