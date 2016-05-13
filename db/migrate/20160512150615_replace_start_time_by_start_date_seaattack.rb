28f5d8c08b28771dda9f7932fa34925d0650b84a
dave <devscrapper@yahoo.fr>
2016-05-13 17:38:23 +0200

    v0.33
    une policy SeaAttack peut commencer à J+2 et non plus next monday

--- db/migrate/20160512150615_replace_start_time_by_start_date_seaattack.rb
+++ db/migrate/20160512150615_replace_start_time_by_start_date_seaattack.rb
@@ -1,6 +1,5 @@
 class ReplaceStartTimeByStartDateSeaattack < ActiveRecord::Migration
   def change
-    change_column :sea_attacks, :start_time, :date
     rename_column :sea_attacks, :start_time, :start_date
   end
 end
