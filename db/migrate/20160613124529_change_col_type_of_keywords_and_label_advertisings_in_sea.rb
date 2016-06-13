c62059f80bdafd794d5e299790f375f46b68fc81
dave <devscrapper@yahoo.fr>
2016-06-13 14:50:01 +0200

    v0.42
    creation d'un Objet SEA => rempalcement de website par sea dans eattack
    possibilité de changer les keywords et les labels_abvdersing en cours d'exéction de la polixy sea_aatack

--- db/migrate/20160613124529_change_col_type_of_keywords_and_label_advertisings_in_sea.rb
+++ db/migrate/20160613124529_change_col_type_of_keywords_and_label_advertisings_in_sea.rb
@@ -1,6 +1,6 @@
 class ChangeColTypeOfKeywordsAndLabelAdvertisingsInSea < ActiveRecord::Migration
   def change
     change_column :seas, :keywords, :text, :null => false
-    change_column :seas, :label_advertising, :text, :null => false
+    change_column :seas, :label_advertisings, :text, :null => false
   end
 end
